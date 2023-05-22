93f269919a61c94a5ec4698fe40168800c545e5a
mstranger <mr.mstranger@gmail.com>
2023-05-22 17:55:10 +0300

    Auth refactoring

--- src/components/auth/SignIn.vue
+++ src/components/auth/SignIn.vue
@@ -1,65 +1,66 @@
 <script setup>
-  import { ref } from "vue"
-  import { useRouter } from "vue-router"
-  import { useAuthStore } from "@/store"
+import { ref } from "vue"
+import { useRouter } from "vue-router"
+import { useAuthStore } from "@/store"
 
-  const error = ref("")
-  const store = useAuthStore()
-  const router = useRouter()
+const error = ref("")
+const store = useAuthStore()
+const router = useRouter()
 
-  const handleSignIn = async (e) => {
-    const emailField = e.target.querySelector("input[name=email]")
-    const passwordField = e.target.querySelector("input[name=password]")
+const handleSignIn = async (event) => {
+  const emailField = event.target.querySelector("input[name=email]")
+  const passwordField = event.target.querySelector("input[name=password]")
 
-    removeInvalidClass(emailField, passwordField)
+  removeInvalidClass(emailField, passwordField)
 
+  try {
     const response = await fetch("http://localhost:3000/auth/login", {
       method: "POST",
-      body: new FormData(e.target)
+      body: new FormData(event.target)
     })
 
     const data = await response.json()
 
-    // TODO: auth here
-
-    // if (!response.ok) {
-    //   console.log(data)
-
-    //   const error = new Error(
-    //     data.message || "Failed to authenticate. Check your login data"
-    //   )
-
-    //   throw error
-    // }
-
-    if (data.error) {
-      emailField.classList.add("invalid")
-      passwordField.classList.add("invalid")
-      error.value = data.error
-    } else {
-      store.login(data.token)
-      router.push({ name: "root" })
+    if (!response.ok) {
+      throw new Error(data.error || "Failed to authenticate")
     }
-  }
 
-  const removeInvalidClass = (...fields) => {
-    fields.forEach(t => t.classList.remove("invalid"))
-    error.value = ""
+    store.login(data.token)
+    router.push({ name: "root" })
+  } catch (e) {
+    emailField.classList.add("invalid")
+    passwordField.classList.add("invalid")
+    error.value = e.message
   }
+}
+
+const removeInvalidClass = (...fields) => {
+  fields.forEach((t) => t.classList.remove("invalid"))
+  error.value = ""
+}
 </script>
 
 <template>
   <div class="d-flex justify-content-center align-items-center h-100 parent">
-    <form class="col-4" @submit.prevent="handleSignIn">
+    <form class="col-10 col-sm-6 col-lg-4" @submit.prevent="handleSignIn">
       <h3 class="mb-3">Sign In</h3>
       <div class="mb-2 text-danger">{{ error }}</div>
 
-      <input type="email" name="email" class="form-control form-control-lg mb-2"
-             placeholder="User name">
-      <input type="password" name="password" class="form-control form-control-lg"
-             placeholder="Password">
+      <input
+        type="email"
+        name="email"
+        class="form-control form-control-lg mb-2"
+        placeholder="User name"
+      />
+      <input
+        type="password"
+        name="password"
+        class="form-control form-control-lg"
+        placeholder="Password"
+      />
 
       <button type="submit" class="btn btn-lg btn-primary my-3">Sign In</button>
+
       <div class="form-text mt-0">
         Don't have an account?
         <router-link to="signup">Sign up</router-link>
@@ -69,7 +70,7 @@
 </template>
 
 <style scoped>
-  .parent {
-    min-height: calc(100vh - 4em);
-  }
+.parent {
+  min-height: calc(100vh - 5rem);
+}
 </style>
--- src/components/auth/SignUp.vue
+++ src/components/auth/SignUp.vue
@@ -1,19 +1,20 @@
 <script setup>
-  import { ref } from "vue"
+import { ref } from "vue"
 
-  const requiredField = ref("")
-  const errors = ref([])
-  const success = ref(false)
+const required = ref("")
+const errors = ref([])
+const success = ref(false)
 
-  const handleSignUp = async (e) => {
-    if (checkEmptyInputs()) {
-      requiredField.value = "The field is required"
-      return false
-    }
+const handleSignUp = async (e) => {
+  if (checkEmptyInputs()) {
+    required.value = "The field is required"
+    return false
+  }
 
-    requiredField.value = ""
-    errors.value = []
+  required.value = ""
+  errors.value = []
 
+  try {
     const response = await fetch("http://localhost:3000/auth/signup", {
       method: "POST",
       body: new FormData(e.target)
@@ -21,71 +22,87 @@
 
     const data = await response.json()
 
-    if (data.errors) {
-      errors.value = data.errors
-      addInvalidClass(data.errors, "email", "password", "password_confirmation")
+    if (!response.ok) {
+      throw new Error(data.errors.join("|") || "Faild to register")
+    }
+
+    success.value = true
+  } catch (e) {
+    errors.value = e.message.split("|")
+    addInvalidClass(errors.value, "email", "password", "password_confirmation")
+  }
+}
+
+const checkEmptyInputs = () => {
+  const inputs = document.querySelectorAll(".form-control")
+
+  let blank = false
+  inputs.forEach((input) => {
+    if (input.value === "") {
+      input.classList.add("invalid")
+      blank = true
     } else {
-      success.value = true
+      input.classList.remove("invalid")
+      blank = false
     }
+  })
+
+  return blank
+}
+
+const addInvalidClass = (errs, ...inputs) => {
+  const starts = {
+    email: "Email",
+    password: "Password does",
+    password_confirmation: "Password confirmation"
   }
 
-  const checkEmptyInputs = () => {
-    const inputs = document.querySelectorAll(".form-control")
-
-    let blank = false
-    inputs.forEach(input => {
-      if (input.value === "") {
-        input.classList.add("invalid")
-        blank = true
-      } else {
-        input.classList.remove("invalid")
-        blank = false
-      }
-    })
-
-    return blank
-  }
-
-  const addInvalidClass = (errs, ...inputs) => {
-    const starts = {
-      "email": "Email",
-      "password": "Password does",
-      "password_confirmation": "Password confirmation"
+  inputs.forEach((input) => {
+    if (errs.some((e) => e.startsWith(starts[input]))) {
+      document.querySelector(`input[name=${input}]`).classList.add("invalid")
     }
-
-    inputs.forEach(input => {
-      if (errs.some(e => e.startsWith(starts[input]))) {
-        document.querySelector(`input[name=${input}]`).classList.add("invalid")
-      }
-    })
-  }
+  })
+}
 </script>
 
 <template>
   <div class="d-flex justify-content-center align-items-center h-100 parent">
-    <form class="col-4" @submit.prevent="handleSignUp">
+    <form class="col-10 col-sm-6 col-lg-4" @submit.prevent="handleSignUp">
       <h3 class="mb-3">Sign Up</h3>
 
       <div class="text-danger">
-        <span>{{ requiredField }}</span>
+        <span>{{ required }}</span>
         <ul class="list-unstyled">
           <li v-for="(error, idx) in errors" :key="idx" class="mb-1">{{ error }}</li>
         </ul>
       </div>
 
       <div class="alert alert-success" v-if="success">
-        <strong>Well done!</strong>
-        You've successfully registered. You can <router-link :to="{ name: 'signin' }">login</router-link> now
+        <strong>Well done!</strong> You've successfully registered. You can
+        <router-link :to="{ name: 'signin' }">login</router-link> now
       </div>
 
-      <input type="email" name="email" class="form-control form-control-lg mb-2"
-             placeholder="User name">
-      <input type="password" name="password" class="form-control form-control-lg mb-2"
-             placeholder="Password">
-      <input type="password" name="password_confirmation" class="form-control form-control-lg"
-             placeholder="Confirm Password">
+      <input
+        type="email"
+        name="email"
+        class="form-control form-control-lg mb-2"
+        placeholder="User name"
+      />
+      <input
+        type="password"
+        name="password"
+        class="form-control form-control-lg mb-2"
+        placeholder="Password"
+      />
+      <input
+        type="password"
+        name="password_confirmation"
+        class="form-control form-control-lg"
+        placeholder="Confirm Password"
+      />
 
       <button type="submit" class="btn btn-lg btn-primary my-3">Sign Up</button>
+
       <div class="form-text mt-0">
         Already a member?
         <router-link to="signin">Sign in</router-link>
@@ -95,7 +112,7 @@
 </template>
 
 <style scoped>
-  .parent {
-    min-height: calc(100vh - 4em);
-  }
+.parent {
+  min-height: calc(100vh - 5em);
+}
 </style>
